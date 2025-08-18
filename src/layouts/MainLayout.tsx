e3334519da5489d93007cf344b6f5d1e2168bb53
najafsikander <najafsikander@yahoo.com>
2025-08-18 18:15:47 +0400

    Fixed the type error in main layout file

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,9 @@
-const MainLayout = ({children}) => {
+import { ReactNode,FC } from "react";
+
+type Props = {
+    children:ReactNode
+}
+const MainLayout:FC<Props> = ({children}) => {
     return(
         <div className="text-center">
       <section className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
