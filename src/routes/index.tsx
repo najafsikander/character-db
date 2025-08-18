9d1c8b1c3504052d1aba2f5dfc68f34816e4b5fe
najafsikander <najafsikander@yahoo.com>
2025-08-18 11:41:13 +0400

    Completed home page for the project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,32 +8,17 @@ export const Route = createFileRoute('/')({
 function App() {
   return (
     <div className="text-center">
-      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-        <img
-          src={logo}
-          className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-          alt="logo"
-        />
-        <p>
-          Edit <code>src/routes/index.tsx</code> and save to reload.
-        </p>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="text-[#61dafb] hover:underline"
-          href="https://tanstack.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn TanStack
-        </a>
-      </header>
+      <section className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+        <h1 className='text-5xl'>Welcome to CharacterDB</h1>
+        <p>Here you can search about certain character from various media content.</p>
+        {/* Images Area */}
+        <section className='flex w-[30%] justify-evenly mt-5'>
+          <img src='/images/rickMorty.jpg' alt='rick and morty avatar' className='w-30 rounded-[60%]'/>
+          <img src='/images/rickMorty.jpg' alt='rick and morty avatar' className='w-30 rounded-[60%]'/>
+          <img src='/images/rickMorty.jpg' alt='rick and morty avatar' className='w-30 rounded-[60%]'/>
+          <img src='/images/rickMorty.jpg' alt='rick and morty avatar' className='w-30 rounded-[60%]'/>
+        </section>
+      </section>
     </div>
   )
 }
