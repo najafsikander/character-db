d836f0d2ebf5d756636812e7f84bdad58305ab54
najafsikander <najafsikander@yahoo.com>
2025-08-20 11:31:22 +0400

    removed comments and fixed loader for query

--- src/routes/rick-morty.tsx
+++ src/routes/rick-morty.tsx
@@ -2,19 +2,15 @@ import Characters from "@/components/Characters";
 import { createFileRoute } from "@tanstack/react-router";
 import { Suspense, useState } from "react";
 
-
-
 export const Route = createFileRoute("/rick-morty")({
   component: RickMortyPage,
 });
 
 function RickMortyPage() {
   const [currentPage, setCurrentPage] = useState<number>(1);
- return (
+  return (
     <Suspense fallback={<div>Loading characters....</div>}>
-      <Characters currentPage={currentPage} setCurrentPage={setCurrentPage}/>
+      <Characters currentPage={currentPage} setCurrentPage={setCurrentPage} />
     </Suspense>
   );
 }
-
-
