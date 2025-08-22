f4c7ba6f597facc54f2f113741ffbb4ee01d23cc
najafsikander <najafsikander@yahoo.com>
2025-08-22 12:31:31 +0400

    Increased rows to 2 from 1 in grid loader

--- src/components/GridLoader.tsx
+++ src/components/GridLoader.tsx
@@ -8,8 +8,8 @@ const GridLoader = () => {
         image: "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png"
     };
     return(<div className="w-screen flex justify-center px-40 py-2 mt-5 mb-5">
-        <section className="grid grid-cols-4 gap-x-5 gap-y-10 mt-5">
-        {Array.from({length:4}, () => loaderObj).map((character:loaderObjType, index:number) => (
+        <section className="grid grid-cols-4 grid-rows-3 gap-x-5 gap-y-10 mt-5">
+        {Array.from({length:8}, () => loaderObj).map((character:loaderObjType, index:number) => (
           <div
             key={index}
             className=" rounded overflow-hidden shadow-2xl"
