19f7496800f368296d77356016e12f2ac6967b50
najafsikander <najafsikander@yahoo.com>
2025-09-11 17:41:36 +0400

    Completed episode detail page for southpark page

--- src/components/SingleEpisode.tsx
+++ src/components/SingleEpisode.tsx
@@ -16,14 +16,27 @@ const SingleEpisode: FC<Props> = ({ url }) => {
   if (isLoading) return <div>Loading...</div>;
 
   console.log("Episode data:", data);
+
+  const getImageUrl = (url: string): string => {
+    if (url.includes("png")) return url.split("png")[0] + "png";
+    if (url.includes("jpg")) return url.split("jpg")[0] + "jpg";
+    if (url.includes("jpeg")) return url.split("jpeg")[0] + "jpeg";
+    return "";
+  };
   return (
     <section className="w-full mx-auto my-auto flex flex-col shadow-2xl">
+      {data?.thumbnail_url && (
+        <img src={getImageUrl(data!.thumbnail_url)} alt={data?.id.toString()} />
+      )}
       <h1>{data?.name}</h1>
-        <div className="flex flex-col gap-3 p-5">
-            <h3>Air Date: {data?.air_date}</h3>
-            <h3>Episode Code: {data?.episode}</h3>
-            <h3>Created At: {new Date(dateString).toLocaleDateString()}</h3>
-        </div>
+      <div className="flex flex-col gap-3 p-5">
+        <p>{data?.description}</p>
+        <h3>Air Date: {data?.air_date}</h3>
+        <h3>Season: {data?.season}</h3>
+        <h3>Episode Code: {data?.episode}</h3>
+        <h3>Created At: {new Date(dateString).toLocaleDateString()}</h3>
+        <h3>Wiki Url: <a href={data?.wiki_url} target="_blank" rel="noopener noreferrer">Click Here</a></h3>
+      </div>
     </section>
   );
 };
